6288fc84badebb74326b311d6e0415f20a181bb3
김지호 <kimjiho@gimjihoui-MacBookAir.local>
2025-04-09 04:35:47 +0900

    컴포넌트 기초

---
+++ 01-components-study/src/final-code/App.jsx
@@ -0,0 +1,13 @@
+import Components from "./components/Components"
+
+function App() {
+
+  return (
+   <div>
+    <h1>여기는 App 컴포넌트 이고요<br />밑에 있는건 Components 컴포넌트입니다.</h1>
+    <Components/>
+   </div>
+  )
+}
+
+export default App
---
+++ 01-components-study/src/final-code/components/Components.jsx
@@ -0,0 +1,11 @@
+function Components() {
+    
+    return (
+     <div>
+        <h1>이건 저의 컴포넌트 입니다.</h1>
+     </div>
+    )
+  }
+  
+  export default Components;
+  
