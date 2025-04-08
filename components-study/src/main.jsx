29f6493d3cb554d1b592bfc8922487514be73198
김지호 <kimjiho@gimjihoui-MacBookAir.local>
2025-04-08 13:46:19 +0900

    핵심 루트

--- components-study/src/main.jsx
+++ components-study/src/main.jsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-
+//학생용 APP 컴포넌트
+import App from './try-it/App.jsx' 
+// 선생용 APP 컴포넌트
+// import App from './final-code/App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
